9e8a994b47012e136f83dfed1ecf8cda29c7d523
Patriks <pmorevs@gmail.com>
2024-04-15 12:27:55 +0300

    1st task LV

--- Main.java
+++ Main.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Uzdevums par vardiem");
-        System.out.println("Ievadi savu vardu");
+        System.out.println("Uzdevums par vārdiem");
+        System.out.println("Ievadi savu vārdu");
         String input = scanner.nextLine();
-        System.out.println("Ievaditais vards ir: " + input);
+        System.out.println("Ievadītais vārds ir: " + input);
     }
 }
+
+//kompilējot izmanto šādu komandu: 
+//  javac -encoding UTF-8 Main.java
