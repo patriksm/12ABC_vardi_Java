d8735708879cc30bc4672996b124894d1c304d17
Patriks <pmorevs@gmail.com>
2024-04-15 12:21:03 +0300

    1st task

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Uzdevums par vardiem");
+        System.out.println("Ievadi savu vardu");
+        String input = scanner.nextLine();
+        System.out.println("Ievaditais vards ir: " + input);
     }
 }
