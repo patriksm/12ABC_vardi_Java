b3b28027335334f09612d6d6c97ede2209d2f19c
Patriks <pmorevs@gmail.com>
2024-04-22 13:29:54 +0300

    done

--- Burti.java
+++ Burti.java
@@ -5,7 +5,16 @@ public class Burti{
         char[] alfabets = alfabetsTXT.toCharArray();
         // System.out.println(teksts.charAt(0));
         // System.out.println(alfabets[0]);
-        System.out.println("Ievadītais vārds ir " + teksts + ", pirmais burts ir " + teksts.charAt(0) + " un tā vieta alfabētā ir ...");
+        Integer pozicija = 0;
+        for(Integer i = 0; i < alfabets.length; i++){
+            //System.out.println(alfabets[i]);
+            if(alfabets[i] == teksts.charAt(0)){
+                pozicija = i + 1;
+                break;
+            }
+        }
+
+        System.out.println("Ievadītais vārds ir " + teksts + ", pirmais burts ir " + teksts.charAt(0) + " un tā vieta alfabētā ir ... " + pozicija);
 
     }
 }
